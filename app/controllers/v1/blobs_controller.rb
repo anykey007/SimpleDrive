5a4000ace56fe4cd71630cae3bc2cd4cc7161700
Andrii Yakushev <anykeyua@gmail.com>
2026-06-16 10:35:51 +0300

    Refactor: - refactor GET /blobs/id

--- app/controllers/v1/blobs_controller.rb
+++ app/controllers/v1/blobs_controller.rb
@@ -40,33 +40,34 @@ module V1
     end
 
     def show
-      external_id = params[:id]
-      blob = @current_user.blobs.find_by!(external_id: external_id)
+      blob = @current_user.blobs.find_by!(external_id: params[:id])
+      data = read_data(blob)
 
+      render json: {
+        id: blob.external_id,
+        data: Base64.strict_encode64(data),
+        size: blob.size_bytes.to_s,
+        created_at: blob.created_at.utc.iso8601
+      }, status: :ok
+
+    rescue ActiveRecord::RecordNotFound => e
+      render json: { error: "Blob not found" }, status: :not_found
+    rescue Storage::ReadDataError => e
+      render json: { error: "File content is missing on storage server: #{e.message}" }, status: :not_found
+    rescue => e
+      render json: { error: "Failed to retrieve storage data: #{e.message}" }, status: :internal_server_error
+    end
+
+    private
+
+    def read_data(blob)
       storage_provider = blob.storage_provider
       adapter = Storage::Factory.build(storage_provider, storage_key: blob.storage_key)
 
-      begin
-        io = adapter.retrieve
-        begin
-          data = io.read
-        ensure
-          io.close if io.respond_to?(:close)
-        end
-
-        render json: {
-          id: blob.external_id,
-          data: Base64.strict_encode64(data),
-          size: blob.size_bytes.to_s,
-          created_at: blob.created_at.utc.iso8601
-        }, status: :ok
-      rescue Storage::ReadDataError => e
-        render json: { error: "File content is missing on storage server: #{e.message}" }, status: :not_found
-      rescue => e
-        render json: { error: "Failed to retrieve storage data: #{e.message}" }, status: :internal_server_error
-      end
-    rescue ActiveRecord::RecordNotFound => e
-      render json: { error: "Blob not found" }, status: :not_found
+      io = adapter.retrieve
+      io.read
+    ensure
+      io.close if io.respond_to?(:close)
     end
   end
 end
